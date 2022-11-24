b954dacce32953b5082c8cae8f19b25052d293b7
wugawuga <dnr3655@naver.com>
2022-11-25 00:40:31 +0900

    feat: 주석, 코드 작성법

--- src/main/ch1/Hello.java
+++ src/main/ch1/Hello.java
@@ -2,6 +2,9 @@ package main.ch1;
 
 public class Hello {
     public static void main(String[] args) {
+        /*
+            주석을 제외한 모든 코드는 클래스의 블럭 {} 내에 작성해야 함
+         */
         System.out.println("Hello");
     }
 }
