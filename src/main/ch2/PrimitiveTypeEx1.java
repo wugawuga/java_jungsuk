3e7ad24d84ef6b328072220ba73b7e67a6ab0fcd
wugawuga <dnr3655@naver.com>
2022-11-25 23:23:11 +0900

    feat: 기본형 타입 boolean

---
+++ src/main/ch2/PrimitiveTypeEx1.java
@@ -0,0 +1,12 @@
+package main.ch2;
+
+public class PrimitiveTypeEx1 {
+    public static void main(String[] args) {
+        boolean power = true;
+        boolean checked = false;
+
+        // 자바에서는 대소문자가 구분되기에 TRUE true 다른 것
+        System.out.println("power = " + power);
+        System.out.println("checked = " + checked);
+    }
+}
