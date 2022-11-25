b0bdd9a2b0d71486cdb73c27ebd5758a96157a55
wugawuga <dnr3655@naver.com>
2022-11-26 00:01:35 +0900

    feat: char 특수문자

--- src/main/ch2/PrimitiveTypeEx2.java
+++ src/main/ch2/PrimitiveTypeEx2.java
@@ -11,5 +11,22 @@ public class PrimitiveTypeEx2 {
 
         // A = 65 (0X41)
         // 가 = 44032 (0XAC00)
+
+        /* 특수문자
+             \t tab
+             \b backspace
+             \f form feed
+             \n new line
+             \r carriage return
+             \\ 역슬래시
+             \' 작은따옴표
+             \" 큰따옴표
+        */
+        System.out.println("======특수문자======");
+        System.out.println('\'');
+        System.out.println("abc\t123\b456");
+        System.out.println('\n');
+        System.out.println("\"Hello\"");
+        System.out.println("c:\\");
     }
 }
